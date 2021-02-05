8d57c7f7f104ae55ab00fe9af87b8cbb37d54c77
Kathrine <kath423a@stud.kea.dk>
2021-02-05 09:15:33 +0100

    javacript code added to user+computer selection

--- script.js
+++ script.js
@@ -16,23 +16,78 @@ function start() {
 function getUserSelection() {
 
     // User clicks on either rock, paper or scissors
-    document.querySelector(".rock").addEventListener("click", makeRandomComputerChoice);
-    document.querySelector(".paper").addEventListener("click", makeRandomComputerChoice);
-    document.querySelector(".scissors").addEventListener("click", makeRandomComputerChoice);
+    document.querySelector(".rock").addEventListener("click", userChoice );
+    document.querySelector(".paper").addEventListener("click", userChoice);
+    document.querySelector(".scissors").addEventListener("click", userChoice);
 }
 
 
+function userChoice() {
+    document.querySelector("#win").classList.add("hidden");
+    document.querySelector("#lose").classList.add("hidden");
+    document.querySelector("#draw").classList.add("hidden");
+    myRandom = Math.floor(Math.random() * 3 + 1);
+    console.log(myRandom);
+
+    if (this.classList.contains("rock")) {
+        document.querySelector("#player1").addEventListener("animationend", function () {
+          document.querySelector("#player1").classList.remove("shake");
+          document.querySelector("#player1").classList.remove("paper");
+          document.querySelector("#player1").classList.remove("scissors");
+          document.querySelector("#player1").classList.remove("rock");
+          document.querySelector("#player1").classList.add("rock");
+        });
+      } else if (this.classList.contains("paper")) {
+        document.querySelector("#player1").addEventListener("animationend", function () {
+          document.querySelector("#player1").classList.remove("shake");
+          document.querySelector("#player1").classList.remove("paper");
+          document.querySelector("#player1").classList.remove("scissors");
+          document.querySelector("#player1").classList.remove("rock");
+          document.querySelector("#player1").classList.add("paper");
+        });
+      } else if (this.classList.contains("scissors")) {
+        document.querySelector("#player1").addEventListener("animationend", function () {
+          document.querySelector("#player1").classList.remove("shake");
+          document.querySelector("#player1").classList.remove("paper");
+          document.querySelector("#player1").classList.remove("scissors");
+          document.querySelector("#player1").classList.remove("rock");
+          document.querySelector("#player1").classList.add("scissors");
+        });
+
+    }
+
+    makeRandomComputerChoice();
+}
+
 function makeRandomComputerChoice() {
     // Computer picks between rock, paper, scissors (random)
-    myRandom = Math.floor(Math.random() * 3 + 1);
-        console.log(myRandom);
-
-    //document.querySelector(".win").classList.add("hidden");
-    //document.querySelector(".lose").classList.add("hidden");
-    //document.querySelector(".draw").classList.add("hidden");
+    if (myRandom == "1") {
+        document.querySelector("#player2").addEventListener("animationend", function () {
+          document.querySelector("#player2").classList.remove("shake");
+          document.querySelector("#player2").classList.remove("paper");
+          document.querySelector("#player2").classList.remove("scissors");
+          document.querySelector("#player2").classList.remove("rock");
+          document.querySelector("#player2").classList.add("rock");
+        });
+      } else if (myRandom == "2") {
+        document.querySelector("#player2").addEventListener("animationend", function () {
+          document.querySelector("#player2").classList.remove("shake");
+          document.querySelector("#player2").classList.remove("paper");
+          document.querySelector("#player2").classList.remove("scissors");
+          document.querySelector("#player2").classList.remove("rock");
+          document.querySelector("#player2").classList.add("paper");
+        });
+      } else if (myRandom == "3") {
+        document.querySelector("#player2").addEventListener("animationend", function () {
+          document.querySelector("#player2").classList.remove("shake");
+          document.querySelector("#player2").classList.remove("paper");
+          document.querySelector("#player2").classList.remove("scissors");
+          document.querySelector("#player2").classList.remove("rock");
+          document.querySelector("#player2").classList.add("scissors");
+        });
+      }
   
-    
-
+   
     showAnimations();
 }
 
